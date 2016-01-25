dcacf47acc2e8d86e186450294d43b557a14dca5
Davide Schiera <davide@sysdig.com>
2016-01-24 17:31:35 -0800

    Step 7: Create index.html
    Sometimes you need an index.html file.
    
    html-webpack-plugin (github.com/ampedandwired/html-webpack-plugin) is what you need. Here we enable the plugin to use a template file and generate the output in the build folder. Many other options are available.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: 'build',
         filename: 'app.js'
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'React - Getting Started',
+            template: './src/index.html'
+        })
+    ]
 };
