c4534819c0759e6136657a4610ffc5e4b7fa8948
Davide Schiera <davide@sysdig.com>
2016-01-24 18:40:36 -0800

    Step 12: Simplify requires.
    You can set the option `resolve.extensions` to tell webpack which extensions to use to resolve modules. `.jsx` is what we need to import JSX files.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Title from './title.jsx'
+import Title from './title'
 
 ReactDOM.render(
     <Title />,
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
-        './src/index.jsx'
+        './src/index'
     ],
     output: {
         path: path.join(__dirname, 'build'),
