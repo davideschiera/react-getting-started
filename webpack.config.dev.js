0d5e0da12066fae39985a77a799772db22f98bb1
Davide Schiera <davide@sysdig.com>
2016-06-02 17:36:07 -0700

    Fixed build (temporarily disabled hot reloading).

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,20 +19,15 @@ config.output.filename = '[name].js';
 config.module.loaders = [
     {
         test: /\.jsx$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
             presets: ['es2015', 'react']
         }
     },
-    // {
-    //     test: /\.jsx$/,
-    //     loader: 'react-hot'
-    // },
     {
         test: /\.js$/,
-        include: [
-            path.join(__dirname, 'src')
-        ],
+        exclude: /node_modules/,
         loaders: [
             'babel?presets[]=es2015'
         ]
